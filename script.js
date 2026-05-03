135964b57d0967aa46bd9a7f3db720be48d99a50
krlitosrs <krlitosrs@gmail.com>
2026-05-03 02:15:14 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCXwjx7-rkh7arUF2ma7rK_gE1luwSB6ic",
   authDomain: "quiniela-app-7cbb0.firebaseapp.com",
-  projectId: "quiniela-app-7cbb0"
+  projectId: "quiniela-app-7cbb0",
 };
 
 firebase.initializeApp(firebaseConfig);
